a3dbc555b0a717b113f2e255333e5fba0f7f8ea3
yunchiapig <carolwi204@gmail.com>
2021-06-27 04:00:52 +0800

    finish search page css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="root">
+      <div className="header">
+        <div className="row">
+          <a className="title">城市辭典</a>
+        </div>
+        <div className="row">
+          <a className="circle-button"></a>
+        </div>
+      </div>
+
+      {/* <div style={{padding_left: '20px'}}>
+        <h1>Responsive Header</h1>
+        <p>Resize the browser window to see the effect.</p>
+        <p>Some content..</p>
+      </div> */}
+      <div id="content">
+        <div className="card">
+          <div className="tags">#tag1 #tag2</div>
+          <div className="vocab">
+            <a className="word">Vocabulary</a>
+          </div>
+          <div className="meaning">explanation........</div>
+          <div className="example">example..........</div>
+          <div className="author"> by me. June 21, 2021</div>
+          <div className="footer"> 讚: 12  倒讚: 3 按鈕:https://material-ui.com/zh/components/button-group/</div>
+        </div>
+      </div>
     </div>
+
+    
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "antd/dist/antd.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink
+} from "@apollo/client";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
+// create an http link for query & mutation:
+const httpLink = new HttpLink({
+  uri: "http://localhost:8000/"
+});
+
+// create a WebSocket link for subscription:
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:8000/", 
+  options: { reconnect: true }
+});
+
+//split link based on operation type
+const link = split( 
+  ({query})=>{
+    const definition = getMainDefinition(query)
+    return(
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore({}) 
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
